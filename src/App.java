dd712853a7418d3083afc11d20ee21e523f61c2e
yousen3438-code <yousen3438@gmail.com>
2025-10-28 07:30:11 +0800

    增加弹窗

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("你好，世界！");
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            javax.swing.JOptionPane pane = new javax.swing.JOptionPane("你好，世界！", javax.swing.JOptionPane.INFORMATION_MESSAGE);
+            javax.swing.JDialog dialog = pane.createDialog(null, "提示");
+            dialog.setModal(false);
+            dialog.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+            dialog.setAlwaysOnTop(true);
+            dialog.setVisible(true);
+            javax.swing.Timer timer = new javax.swing.Timer(5000, e -> dialog.dispose());
+            timer.setRepeats(false);
+            timer.start();
+        });
     }
 }
