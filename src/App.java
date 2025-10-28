04bdae7955a895fe7c2ac65a9dbde5d65e82a797
yousen3438-code <yousen3438@gmail.com>
2025-10-29 02:36:11 +0800

    刷新测试

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("你好，世界！");
+        System.out.println("提示");
         javax.swing.SwingUtilities.invokeLater(() -> {
             javax.swing.JOptionPane pane = new javax.swing.JOptionPane("你好，世界！", javax.swing.JOptionPane.INFORMATION_MESSAGE);
             javax.swing.JDialog dialog = pane.createDialog(null, "提示");
